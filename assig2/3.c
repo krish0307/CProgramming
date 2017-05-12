a539769ac9676653929b76a66d04fa5611bf755f
krish0307 <tkrishna0307@gmail.com>
2017-05-12 16:55:35 +0530

    Update 3.c

--- assig2/3.c
+++ assig2/3.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-	int a=1+rand()%11;
-	if(a<3){
+void fun(int n)
+{
+    if(n==0)
+        return;
+    else if(n>0){
+            int a=1+rand()%10;
+        if(a<3){
 		printf("a\n");
 	}else if(a<6){
 		printf("b\n");
@@ -11,5 +15,10 @@ int main(){
 	}else{
 		printf("d\n");
 	}
+	fun(--n);
+    }
+}
+int main(){
+	fun(10);
 	return 0;
 }
